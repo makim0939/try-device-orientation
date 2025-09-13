edb2284e28fc9a962dabb60f385a92212879d539
makim0939 <makim0939@gmail.com>
2025-09-14 07:17:36 +0900

    3Dオブジェクトの回転にイージングを適用

--- src/hooks/useDeviceOrientation.ts
+++ src/hooks/useDeviceOrientation.ts
@@ -14,12 +14,20 @@ export function useDeviceOrientation() {
 	});
 
 	useEffect(() => {
+		const makeSmooth = (prev: number, current: number, factor: number) => {
+			return prev + (current - prev) * factor;
+		};
 		const handleOrientation = (event: DeviceOrientationEvent) => {
-			setOrientation({
+			const orientation = {
 				alpha: event.alpha ?? 0,
 				beta: event.beta ?? 0,
 				gamma: event.gamma ?? 0,
-			});
+			};
+			setOrientation((prev) => ({
+				alpha: makeSmooth(prev.alpha, orientation.alpha, 0.05),
+				beta: makeSmooth(prev.beta, orientation.beta, 0.05),
+				gamma: makeSmooth(prev.gamma, orientation.gamma, 0.05),
+			}));
 		};
 
 		window.addEventListener("deviceorientation", handleOrientation, true);
